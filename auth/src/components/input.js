4a15acde4bcb123807a2b6fe32034ac647607c40
Sang Choi <akasyashc@gmail.com>
2017-02-18 20:42:31 -0800

    Add login input style

--- auth/src/components/input.js
+++ auth/src/components/input.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { TextInput, View, Text, StyleSheet } from 'react-native';
 
 const Input = ({ label, value, onChangeText }) => {
+const { textInputStyle, labelStyle, containerStyle } = styles;
+
     return (
-        <View>
-            <Text>{label}</Text>
+        <View style={containerStyle}>
+            <Text style={labelStyle}>{label}</Text>
             <TextInput
-                style={styles.textInputStyle}
+                style={textInputStyle}
                 value={value}
                 onChangeText={onChangeText}
              />
@@ -16,8 +18,23 @@ const Input = ({ label, value, onChangeText }) => {
 
 const styles = StyleSheet.create({
     textInputStyle: {
-        height: 20,
-        width: 300
+        paddingRight: 5,
+        paddingLeft: 5,
+        color: '#000',
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
+    },
+    containerStyle: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
     }
 });
 
