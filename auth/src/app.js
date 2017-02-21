5dfb311b38d7161b83bbd5ec86343ea254d43fde
Sang Choi <akasyashc@gmail.com>
2017-02-20 21:21:01 -0800

    Initialize onAuthStateChanged hanlder on app.js

--- auth/src/app.js
+++ auth/src/app.js
@@ -4,6 +4,8 @@ import firebase from 'firebase';
 import { Header, Login } from './components';
 
 class App extends Component {
+    state = { loggedIn: false };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: "AIzaSyCJXrN1LhK91GeiTp2AxEfiV_LCZP4f7CE",
@@ -11,6 +13,14 @@ class App extends Component {
             databaseURL: "https://auth-e7883.firebaseio.com",
             storageBucket: "auth-e7883.appspot.com",
             messagingSenderId: "801227475796"
+      });
+
+      firebase.auth().onAuthStateChanged((user) => {
+          if (user) {
+              this.setState({ loggedIn: true });
+          } else {
+              this.setState({ loggedIn: false });
+          }
       });
     }
 
