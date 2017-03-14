14a9235a91ae11dc47c2f217d8357f81ecce2114
Sang Choi <akasyashc@gmail.com>
2017-03-13 21:38:40 -0700

    Redux setup

--- techstack/src/app.js
+++ techstack/src/app.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import devToolsEnhancer from 'remote-redux-devtools';
+
+import reducers from './reducers';
 
 const App = () => {
     return (
-        <View />
+        <Provider store={createStore(reducers, devToolsEnhancer())}>
+            <View />
+        </Provider>
     );
 };
 
